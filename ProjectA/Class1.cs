4f743b6846c5ccf3ef3d80e622cad52f25dc4a62
zainabzulfiqar <shahzainab952@gmail.com>
2023-08-26 16:18:22 +0500

    initial commit

---
+++ ProjectA/Class1.cs
@@ -0,0 +1,14 @@
+﻿using ProjectB;
+
+namespace ProjectA
+{
+    public class Class1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; } 
+        public string Type { get; set; }    
+        public Class2 cll3 { get; set; }
+
+    }
+}
---
+++ ProjectB/Class2.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectB
+{
+    public class Class2
+    {
+        public Class2() { }
+        public int a {  get; set; } 
+        public int b { get; set; }
+
+    }
+}
